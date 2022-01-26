af7dc8db8d49ded8af86ad19d14883e50f08a764
Tamara <tamara.herashchenko@gmail.com>
2022-01-26 14:55:01 +0200

    Fix animation of section .menu

--- src/js/script.js
+++ src/js/script.js
@@ -3,23 +3,40 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const menu = document.querySelector('.menu__block'),
           trigger = document.querySelector('.menu__close'),
+          menuOverlay = document.querySelector('.menu__overlay'),
           menuNav = document.querySelector('.menu__nav');
         
+    function showContent(item) {
+        item.classList.add('show');
+        item.classList.remove('hide');
+    }
+    function hideContent(item) {
+        item.classList.add('hide');
+        item.classList.remove('show');
+    }
+    function closeMenu() {
+        menu.classList.remove('menu__block_active', 'wow', 'animate__animated', 'animate__zoomIn');
+        trigger.classList.remove('menu__close_active');
+        
+        hideContent(menuNav);
+        hideContent(menuOverlay);
+    }
 
     trigger.addEventListener('click', () => {
         if (!menu.classList.contains('menu__block_active') && !trigger.classList.contains('menu__close_active')) {
             menu.classList.add('menu__block_active', 'wow', 'animate__animated', 'animate__zoomIn');
-            trigger.classList.add('menu__close_active', 'wow', 'animate__animated', 'animate__pulse');
-            menuNav.classList.add('show');
-            menuNav.classList.remove('hide');
-        } else {
-            menu.classList.remove('menu__block_active', 'wow', 'animate__animated', 'animate__zoomIn');
-            trigger.classList.remove('menu__close_active', 'wow', 'animate__animated', 'animate__zoomIn');
+            trigger.classList.add('menu__close_active');
+
+            menuNav.classList.add('wow', 'animate__animated', 'animate__bounceInRight');
+            showContent(menuNav);
+            showContent(menuOverlay);
             
-            menuNav.classList.add('hide');
-            menuNav.classList.remove('show');
+            menuOverlay.addEventListener('click', () => {
+                closeMenu();
+            }) ;
+        } else {
+            closeMenu();
         }
     });
-    
 
 });
